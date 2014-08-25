a6033453126578f59c64a29163e48de5b7b1829b
mynomoto <mynomoto@gmail.com>
2014-08-25 13:16:30 -0300

    Update ring

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "http://example.com/FIXME"
 
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [ring/ring "1.3.0"]
+                 [ring/ring "1.3.1"]
                  [compojure "1.1.8"]
                  [fogus/ring-edn "0.2.0"]]
 
